984fb421aea6a5c29fa7b482f7f9b54adb219d1e
sean <sean@pavilion>
2017-08-16 22:27:30 +0100

    add app bootstrap

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sean
+ * Date: 15/08/17
+ * Time: 20:50
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Response;
+
+// bootstrap silex
+$app = new Application();
+$app['debug'] = true;
+
+$app->get('/', function() {
+    return new Response('HELLO WORLD!');
+});
+
+$app->run();
